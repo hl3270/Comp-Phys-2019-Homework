6e87639c6f0db94554c11e2147f1d612cd5efae6
hl3270 <45616242+hl3270@users.noreply.github.com>
2019-12-05 12:58:34 -0500

    Add files via upload

---
+++ Comp Phys Hw2/Hw_2-1.c
@@ -0,0 +1,108 @@
+/*Hw_2-1.c*/
+
+/*
+Exercise 6.11 (Computational Physics, Mark Newman)
+Overrelaxation
+
+If you did not already do Exercise 6.10, you should do it before this one.
+The ordinary relaxation method involves iterating the equation x′ = f(x), starting from an initial guess, until it converges.
+As we have seen, this is often a fast and easy way to find solutions to nonlinear equations.
+However, it is possible in some cases to make the method work even faster using the technique of overrelaxation.
+Suppose our initial guess at the solution of a particular equation is, say, x = 1, and the final, true solution is x = 5.
+After the first step of the iterative process, we might then see a value of, say, x = 3.
+In the overrelaxation method, we observe this value and note that x is increasing,
+then we deliberately overshoot the calculated value, in the hope that this will get us closer to the final solution
+- in this case we might pass over x = 3 and go straight to a value of x = 4 perhaps,
+which is closer to the final solution of x = 5 and hence should get us to that solution quicker.
+The overrelaxation method provides a formula for performing this kind of overshooting in a controlled fashion and often,
+though not always, it does get us to our solution faster. In detail, it works as follows.
+
+We can rewrite the equation x′ = f(x) in the form x′ = x + \Delta x, where
+
+\Delta x = x′ - x = f(x) - x.
+
+The overrelaxation method involves iteration of the modified equation
+
+x′ = x + (1 + \omega) \Delta x,
+
+(keeping the definition of \Delta x the same). If the parameter \omega is zero, then this is the same as
+the ordinary relaxation method, but for \omega > 0 the method takes the amount \Delta x by which the value of x
+would have been changed and changes by a little more. Using \Delta x = f(x) - x, we can also write x′ as
+
+x′ = x + (1 + \omega) * [f (x) - x] = (1 + \omega) f(x) - \omega x,
+
+which is the form in which it is usually written.
+
+For the method to work the value of \omega must be chosen correctly, although there is some wiggle room
+- there is an optimal value, but other values close to it will typically also give good results.
+Unfortunately, there is no general theory that tells us what the optimal value is.
+Usually it is found by trial and error.
+
+a) Derive an equivalent of
+
+\epsilon′ = \epsilon * f′(x*), x* = x′ + \epsilon′
+
+for the overrelaxation method and hence show that the error on x′, the equivalent of
+
+\epsilon′ = (x - x′) / (1 - 1 / f′(x*) \approx (x - x′) / (1 - 1 / f′(x)
+
+, is given by
+
+\epsilon′ \approx (x - x′ / (1 - 1 / ((1 + \omega) * f′(x) - \omega))
+
+b) Consider again the equation x = 1 - e ^ (- c * x) that we solved in Exercise 6.10.
+Take the program you wrote for part (a) of that exercise, which solved the equation for the case c = 2,
+and modify it to print out the number of iterations it takes to converge to a solution accurate to 10^(-6).
+
+c) Now write a new program (or modify the previous one) to solve the same equation x = 1 - e ^ (- c * x) for c = 2,
+again to an accuracy of 10^(-6), but this time using overrelaxation.
+Have your program print out the answers it finds along with the number of iterations it took to find them.
+Experiment with different values of \omega to see how fast you can get the method to converge.
+A value of \omega = 0.5 is a reasonable starting point.
+With some trial and error you should be able to get the calculation to converge about twice as fast as
+the simple relaxation method, i.e., in about half as many iterations.
+
+d) Are there any circumstances under which using a value \omega < 0 would help us find a solution faster than
+we can with the ordinary relaxation method? (Hint: The answer is yes, but why?)
+*/
+
+#include <stdlib.h>     //standard library
+#include <stdio.h>      //standard input/output library
+#include <string.h>     //string routines
+#include <math.h>       //math routines
+
+#include "relaxation.h" //relaxation method header file
+
+void main()
+{
+    float f(float x);                //right hand side of the equation x = f(x)
+    float relaxation(float x_0, float (*func_ptr)(float x), float accuracy);  //standard relaxation method
+
+    float x_0;                       //starting point
+    float x;
+    float (*func_ptr)(float x);
+    float accuracy;                  //target accuracy
+
+    func_ptr = f;
+    strcpy(equation_name, "x=1-exp(-2x)");
+    printf("Solution to %s using relaxation method\n\n", equation_name);
+    //printf("Enter the starting point x_0 = ");                         //enter manually the starting point and the target accuracy
+    //scanf("%f", &x_0);
+    //printf("\nEnter the target solution accuracy \\epsilon = ");
+    //scanf("%f", &accuracy);
+    //printf("\n");
+    x_0 = 1;                                                  //predefined starting point
+    accuracy = 1e-6;                                          //predefined target accuracy
+    printf("x_0 = %7.6e, accuracy = %7.6e\n", x_0, accuracy);
+
+    x = relaxation(x_0, func_ptr, accuracy);
+    printf("\nx = %7.6e\n\n", x);
+
+    printf("Solution to %s using overrelaxation method\n\n", equation_name);
+    x = overrelaxation(x_0, func_ptr, accuracy);
+}
+
+float f(float x)
+{
+    return (1 - exp(- 2 * x));
+}
---
+++ Comp Phys Hw2/relaxation.h
@@ -0,0 +1,107 @@
+/*
+relaxation.h
+
+Solution of nonlinear equations using relaxation methods.
+
+Header files required:
+#include <stdlib.h>  //standard library
+#include <stdio.h>   //standard input/output library
+#include <string.h>  //string routines
+#include <math.h>    //math routines
+*/
+
+char equation_name[64];
+
+/*
+Solving one dimensional nonlinear equation x = f(x) using relaxation method : new x' = f(x) until convergence.
+Error \epsilon \approx (x - x') / (1 - 1 / f'(x))
+It is required |f'(x*)| < 1 to converge.
+*/
+float relaxation(float x_0, float (*func_ptr)(float x), float accuracy)
+{
+    float x;            //old estimate of x
+    float x_prime;      //new estimate of x
+    float epsilon;      //approximate error
+    int iter;           //number of iterations
+    FILE *fp;
+    char filename[64];
+
+    sprintf(filename, "relax_of_%s.txt", equation_name);
+    fp = fopen(filename, "w");
+    fprintf(fp, "Solution to %s using relaxation method\n\n");
+    fprintf(fp, "x_0 = %7.6e\n\n", x_0);
+    fprintf(fp, "iter     x        \n");
+
+    iter = 1;
+    x = (*func_ptr)(x_0);
+    epsilon = (x - x_0) / (1 - (x - x_0) / ((*func_ptr)(x) - x));  //x = f(x_0)
+    printf("  i         x          \n");
+    printf("%3d %7.6e\n", iter, x);
+    fprintf(fp, "%3d %7.6e\n", iter, x);
+    iter++;
+
+    for(; fabs(epsilon) >= accuracy; iter++)
+    {
+        x_prime = (*func_ptr)(x);
+        epsilon = (x_prime - x) / (1 - (x_prime - x) / ((*func_ptr)(x_prime) - x_prime));  //x' = f(x)
+        x = x_prime;
+        printf("%3d %7.6e\n", iter, x);
+        fprintf(fp, "%3d %7.6e\n", iter, x);
+    }
+
+    fprintf(fp, "\nx = %7.6e\n\n", x);
+
+    fclose(fp);
+
+    return x;
+}
+
+/*
+Solving one dimensional nonlinear equation x = f(x) using overrelaxation method
+
+x' = (1 + \omega)f(x) - \omega * x
+
+\epsilon \approx (x - x') / (1 - 1 / [(1 + \omega) * f'(x) - \omega])
+
+This function will give the result for some different \omega's and also return the number of iterations
+for every \omega to reach the target accuracy
+*/
+float overrelaxation(float x_0, float (*func_ptr)(float x), float accuracy)
+{
+    float x;            //old estimate of x
+    float x_prime;      //new estimate of x
+    float epsilon;      //approximate error
+    int iter;           //number of iterations
+    float omega;        //overrelaxation parameter
+    FILE *fp;
+    char filename[64];
+
+    sprintf(filename, "overrelax_of_%s.txt", equation_name);
+    fp = fopen(filename, "w");
+    fprintf(fp, "Solution to %s using overrelaxation method\n\n");
+    fprintf(fp, "x_0 = %7.6e\n\n", x_0);
+    fprintf(fp, "iter     \\omega        x        \n");
+    printf("iter     \\omega        x        \n");
+
+    for(omega = 0; omega < 1; omega += 0.01)
+    {
+        iter = 1;
+        x = (*func_ptr)(x_0) * (1 + omega) - omega * x_0;
+        epsilon = (x - x_0) / (1 - 1 / ((1 + omega) * ((*func_ptr)(x) - x) / (x - x_0) - omega));  //x = f(x_0)
+        iter++;
+
+        for(; fabs(epsilon) >= accuracy; iter++)
+        {
+            x_prime = (*func_ptr)(x) * (1 + omega) - omega * x;
+            epsilon = (x_prime - x) / (1 - 1 / ((1 + omega) * ((*func_ptr)(x_prime) - x_prime) / (x_prime - x) - omega));  //x' = f(x)
+            x = x_prime;
+        }
+
+        printf("%3d %7.6e %7.6e\n", iter - 1,omega, x);
+        fprintf(fp, "%3d %7.6e %7.6e\n", iter - 1,omega, x);
+    }
+
+    fclose(fp);
+
+    return x;
+}
